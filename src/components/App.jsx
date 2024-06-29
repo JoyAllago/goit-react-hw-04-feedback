8c791d62ebc48b569ff7173801d06ccf5a645e28
enquire07 <145021641+enquire07@users.noreply.github.com>
2024-06-29 09:02:37 +0800

    updated

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Notification } from './Notification/Notification';
 import { Statistics } from './Statistics/Statistics';
-import { FeedbackOptions } from './FeedbackOPtions/FeedbackOptions';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Section } from './Section/Section';
 
 export class App extends Component {
